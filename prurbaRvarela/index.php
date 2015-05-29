2276915f38497be97604584875ceb1ee4be3ea6d
rvarela <rvarela@ubiobio.cl>
2015-05-29 17:06:18 -0300

    SOLO Rama NUEVA LINEA

--- prurbaRvarela/index.php
+++ prurbaRvarela/index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
         echo "PRUEBAS RAMAS DIFF";
+        echo "Nueva linea";
         ?>
     </body>
 </html>
